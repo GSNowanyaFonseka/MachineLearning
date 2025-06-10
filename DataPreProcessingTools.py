684c383dc3da3d2c5479f189114772a8244568be
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-06-10 16:25:01 +0530

    feat: importing necessary libraries

---
+++ DataPreProcessingTools.py
@@ -0,0 +1,5 @@
+# importing libraries
+import numpy as np   #library in python for numerical and scientific computing
+import matplotlib.pyplot as pit   #library in python for plotting graphs
+import pandas as pd   #library in python for data manipulation and analysis
+
