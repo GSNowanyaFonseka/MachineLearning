23fcca135e2caf9b953269877df07d003c052ed0
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-06-10 17:44:48 +0530

    feat: handle missing data with mean imputation

--- DataPreProcessingTools.py
+++ DataPreProcessingTools.py
@@ -11,3 +11,10 @@ y = dataset.iloc[:,-1].values  #getting the last column    vector of labels Y
 print(x)
 print(y)
 
+#Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:,1:3])
+x[:, 1:3] = imputer.transform(x[:,1:3])
+
+print(x)
